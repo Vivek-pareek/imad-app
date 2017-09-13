86e67d3555e2f38f88b424fd81655002f058f25d
Vivek-pareek <pareekvivek123@gmail.com>
2017-09-13 21:37:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ button.onClick=function(){
            if(request.status===200)
            {
                var counter=request.responseText;
-               var span=document.getElementById("count");
+               var span=document.getElementById('count');
                span.innerHtml=counter.toString();
            }
        }
