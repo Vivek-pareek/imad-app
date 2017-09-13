33c4918a648c8b8547854b1bdf1a0c852968c181
Vivek-pareek <pareekvivek123@gmail.com>
2017-09-13 21:33:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
-console.log('Loaded!');
+var button=document.getElementById("counter");
+button.onClick=function(){
+    var request=new XMLHttpRequest();
+    request.onReadyStateChange=function(){
+       if(request.readyState===XMLHttpRequest.DONE)
+       {
+           if(request.status===200)
+           {
+               var counter=request.responseText;
+               var span=document.getElementById("count");
+               span.innerHtml=counter.toString();
+           }
+       }
+    };
+    request.open('GET','http://pareekvivek123.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
